8af347d5e44c341ee114569f07d795d6ec89519a
Jenifer <mexajeni@gmail.com>
2024-02-13 11:35:31 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".message").textContent = "🎊 Correct Number!";
+
+console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".number").textContent = 13;
+
+document.querySelector(".score").textContent = 22;
+
+console.log(document.querySelector(".guess").value);
+
+document.querySelector(".guess").value = 23;
+
+console.log(document.querySelector(".guess").value);
